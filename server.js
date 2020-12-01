9065fc12510b342f67e22b7383304ff58bfe1ff6
Atul Ranjan <atulranjan.1311@gmail.com>
2020-12-01 19:20:29 +0530

    auth system built

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 // database connection
 
@@ -22,7 +23,9 @@ connection.once('open', () => {
     console.log('Database connected...');            //Databe connection snippet
 }).catch(err => {
     console.log('Connection failed...')
-});                                             
+});        
+
+
 
 //session store
 
@@ -40,17 +43,25 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } //24hours age of cookie
 }))
 
+//passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 
 // Assets
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 //global middleware
 
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
  
